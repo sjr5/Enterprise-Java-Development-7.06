702992ba73fefae4747933aaf7627684e12ad47c
saleh <sale7_sh507@icloud.com>
2023-11-15 13:32:24 +0300

    first commit

--- src/app/components/todolist/todolist.component.ts
+++ src/app/components/todolist/todolist.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class TodolistComponent {
 
+  newTodo: string = '';
+  todos: Todo[] = [];
+
+  addTodo() {
+    if (this.newTodo.trim() !== '') {
+      this.todos.push({ text: this.newTodo, completed: false, postponed: false });
+      this.newTodo = '';
+    }
+  }
+
+  markComplete(todo: Todo) {
+    todo.completed = !todo.completed;
+  }
+
+  postpone(todo: Todo) {
+    todo.postponed = !todo.postponed;
+  }
+
+  delete(todo: Todo) {
+    this.todos = this.todos.filter(t => t !== todo);
+  }
+
+  clean() {
+    this.todos = this.todos.filter(t => !t.completed);
+  }
+
+  restore() {
+    this.todos.forEach(t => (t.postponed = false));
+  }
 }
+
+interface Todo {
+  text: string;
+  completed: boolean;
+  postponed: boolean;
+}
+
